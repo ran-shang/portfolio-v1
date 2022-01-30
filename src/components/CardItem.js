eb84def84c1181e51f131acc7127a75651f6907e
ran-shang <48609368+ran-shang@users.noreply.github.com>
2022-01-29 22:24:54 -0500

    card links

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,7 +5,7 @@ function CardItem(props) {
     return (
         <>
           <li className='cards__item'>
-              <Link to='/' className='cards__item__link'>
+              <Link to={props.path} className='cards__item__link'>
                   <figure className='cards__item__pic-wrap' data-category={props.label}>
                     <img 
                     src={props.src} 
